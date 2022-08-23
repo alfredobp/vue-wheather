744fac6585430f7568c3968f940368d5705b8ad9
alfredobp <alfredobape@gmail.com>
2022-08-23 08:28:01 +0200

    new url

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:alfredobp/wheather-info.git master:gh-pages
-git push -f https://github.com/alfredobp/wheather-info.git master:gh-pages
+git push -f https://github.com/alfredobp/vue-wheather.git master:gh-pages
 
 
 cd -
